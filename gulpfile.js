6e75d97a44cb4feace57608c3005a8e6f612831b
Mike Rothschild <mike.a.rothschild@att.net>
2015-11-02 00:03:51 -0500

    Take the node server out of the gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,11 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
-var exec = require('child_process').exec;
  
-gulp.task('server', function(ser) {
+gulp.task('server', function() {
   gulp.src('app')
     .pipe(webserver({
       livereload: true,
       directoryListing: false,
       open: true
     }));
-
-    exec('node server.js', function (err, stdout, stderr) {
-	    console.log(stdout);
-	    console.log(stderr);
-	    ser(err);
-  	});
 });
